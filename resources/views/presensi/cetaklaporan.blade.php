1790dbaddbd7aab6d810a8e7e33cad9190ac3203
William Rusli <wrusli75@gmail.com>
2023-07-20 16:15:52 +0700

    check mark for rekap

--- resources/views/presensi/cetaklaporan.blade.php
+++ resources/views/presensi/cetaklaporan.blade.php
@@ -88,8 +88,10 @@
               for ($i=1; $i <= 31; $i++) { 
                 $tgl = 'tgl_' . $i;
             ?>
-              <td style='background-color: <?php if ($d->$tgl != null){ ?> green; <?php } ?>'></td>
-              
+              <td> <?php if ($d->$tgl != null){ ?> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="12" height="12" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l5 5l10 -10"></path>
+             </svg> <?php } ?></td>
             <?php
               }
             ?>
